47243e0e18f136b9569acdc584977da1bbe9ab3d
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2017-10-08 21:01:06 -0300

    1.5 - Seeders

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,18 @@
 |
 */
 
-$factory->define(CodeDelivery\User::class, function (Faker\Generator $faker) {
+$factory->define(CodeDelivery\Models\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+
+$factory->define(CodeDelivery\Models\Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
     ];
 });

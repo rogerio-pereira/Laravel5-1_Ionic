0fd95770d3d8404772212c06547103596323d160
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2017-10-08 22:25:44 -0300

    2.3 - Criando Repositories

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,22 @@
 
 namespace CodeDelivery\Models;
 
-use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract,
+class User extends Model implements Transformable,
+                                    AuthenticatableContract,
                                     AuthorizableContract,
                                     CanResetPasswordContract
 {
-    use Authenticatable, Authorizable, CanResetPassword;
+    use TransformableTrait, Authenticatable, Authorizable, CanResetPassword;
 
     /**
      * The database table used by the model.
@@ -41,4 +44,5 @@ class User extends Model implements AuthenticatableContract,
     {
         return $this->hasOne(Client::class);
     }
+
 }

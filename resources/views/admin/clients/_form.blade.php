26a9a56f90df8713b2d64ea544f2dd73bddfbf34
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2017-10-09 17:31:40 -0300

    5.3 - Editando Client

--- resources/views/admin/clients/_form.blade.php
+++ resources/views/admin/clients/_form.blade.php
@@ -1,4 +1,34 @@
 <div class='form-group'>
-    {!! Form::label('name', 'Nome:') !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    {!! Form::label('user[name]', 'Nome:') !!}
+    {!! Form::text('user[name]', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class='form-group'>
+    {!! Form::label('user[email]', 'Email:') !!}
+    {!! Form::email('user[email]', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class='form-group'>
+    {!! Form::label('phone', 'Telefone:') !!}
+    {!! Form::text('phone', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class='form-group'>
+    {!! Form::label('address', 'Endereço:') !!}
+    {!! Form::textarea('address', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class='form-group'>
+    {!! Form::label('city', 'Cidade:') !!}
+    {!! Form::text('city', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class='form-group'>
+    {!! Form::label('state', 'Estado:') !!}
+    {!! Form::text('state', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class='form-group'>
+    {!! Form::label('zipcode', 'CEP:') !!}
+    {!! Form::text('zipcode', null, ['class' => 'form-control']) !!}
 </div>
--- resources/views/admin/clients/edit.blade.php
+++ resources/views/admin/clients/edit.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <div class='container'>
-        <h3>Editando Categoria: {{$category->name}}</h3>
+        <h3>Editando Cliente: {{$client->user->name}}</h3>
 
         @include('errors._check')
 
-        {!! Form::model($category, ['route' => ['admin.categories.update', $category->id]]) !!}
-            @include('admin.categories._form')
+        {!! Form::model($client, ['route' => ['admin.clients.update', $client->id]]) !!}
+            @include('admin.clients._form')
 
             <div class='form-group'>
                 {!! Form::submit('Salvar', ['class' => 'btn btn-primary']) !!}

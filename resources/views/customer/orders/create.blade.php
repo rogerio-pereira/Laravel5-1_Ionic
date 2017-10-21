6a17a6e816c08233bb11f725dd14464034928155
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2017-10-21 09:49:21 -0200

    7.6 - Criando Form de pedidos - Parte 2

--- resources/views/customer/orders/create.blade.php
+++ resources/views/customer/orders/create.blade.php
@@ -10,7 +10,7 @@
             <div class='form-group'>
                 <label>Total:</label>
                 <p id='total'></p>
-                <a href='#' class='btn btn-default'>Novo Item</a>
+                <a href='#' id='btnNewItem' class='btn btn-default'>Novo Item</a>
 
                 <table class="table table-bordered">
                     <thead>
@@ -43,4 +43,25 @@
             </div>
         {!! Form::close() !!}
     </div>
+@endsection
+
+@section('post-scripts')
+    <script>
+        $('#btnNewItem').click(function(){
+            var row = $('table tbody > tr:last'), 
+                newRow = row.clone(),
+                length = $('table tbody tr').length;
+
+            newRow.find('td').each(function() {
+                var td = $(this),
+                    input = td.find('input,select'),
+                    name = input.attr('name');
+
+                input.attr('name', name.replace((length-1) + '', length + ''));
+            });
+
+            newRow.find('input').val(1);
+            newRow.insertAfter(row);
+        });
+    </script>
 @endsection
